733a5083c25401c345ee1cc1a44bcd2468e9566d
AntPtr <58531360+AntPtr@users.noreply.github.com>
2025-08-13 17:11:23 +0200

    Update RemoveDuplicateSortedArray.cpp
    Added another variant

--- RemoveDuplicateSortedArray.cpp
+++ RemoveDuplicateSortedArray.cpp
@@ -1,3 +1,4 @@
+//Optimized solution
 int removeDuplicates(int* nums, int numsSize) 
 {
   if (numsSize == 0) return 0;
@@ -12,4 +13,28 @@ int removeDuplicates(int* nums, int numsSize)
     }
   }
   return writeIndex;
+}
+
+//Raw solution
+int removeDuplicates(int* nums, int numsSize) 
+{
+  int size = numsSize;
+
+  for (int i = 0; i < size; i++) 
+  {
+    for (int k = i + 1; k < size; k++) 
+    {
+      if (nums[i] == nums[k]) 
+      {
+        // Shift elements to the left
+        for (int j = k; j < size - 1; j++) 
+        {
+          nums[j] = nums[j + 1];
+        }
+        size--;
+        k--; 
+      }
+    }
+  }
+  return size;
 }
